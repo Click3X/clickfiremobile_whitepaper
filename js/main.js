4c0b3e9cc29197c868063b169011678fd511b46d
Owen Corso <owen@click3x.com>
2013-09-26 11:44:37 -0400

    remove pdf tracking cause we have a download landing page now

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ jQuery( function($){
 	console.log("welcome to the show.");
 
 	$("a:not(.pdf-download)").click(function($e){ _gaq.push(['_link', $(this).attr('href')]); return false;});
-	$("a.pdf-download").click(function($e){ _gaq.push(['_trackEvent', 'O Red White Paper Events', 'PDF Download', 'White Paper PDF Download']); });
+	
+	//oc: not needed because now we have a landing page for the file download.
+	//$("a.pdf-download").click(function($e){ _gaq.push(['_trackEvent', 'O Red White Paper Events', 'PDF Click Download', 'White Paper PDF Download']); });
 
 });
